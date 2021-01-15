966478f4987f548f05b54b8b4e72580404c4c793
AndiZm <34646647+AndiZm@users.noreply.github.com>
2021-01-15 11:47:34 +0100

    Add files via upload

---
+++ auto_analysis_GUI/aa_windowtest.py
@@ -0,0 +1,39 @@
+import subprocess
+from pathlib import Path
+from tqdm import tqdm
+import numpy as np
+import os
+
+zeroadder = []
+zeroadder.append("dummy")
+zeroadder.append("0000") # einstellig
+zeroadder.append("000")  # zweistellig
+zeroadder.append("00")   # dreistellig
+zeroadder.append("0")    # vierstellig
+zeroadder.append("")     # fuenfstellig
+
+
+def execute(infiles, outfilepath, shifts, offset, packetlength, npackets, threshold):
+	#print ("\nThreshold: "+ str(threshold))
+	#print ("Shifts: " + str(shifts))
+	#print ("Offset: " + str(offset))
+	#print ("Packetlength: " + str(packetlength))
+	#print ("npackets: " + str(npackets))
+
+	commands = []
+
+	for i in range (0, len(infiles)):
+		infile = infiles[i]
+		outfile = infile.split("/")[-1]
+		outfile = outfile[:-3] + "txt"
+		resultpath = outfilepath + "/" + outfile		
+
+		#print (infile); print ("\t" + resultpath)
+
+		commands.append("python.exe ../pur_bin/curcor_int8_cpu.py -i " + infile + " -f " + resultpath + " -a " + str(threshold) + " -b " + str(threshold) + " -s " + str(shifts) + " -o " + str(offset) + " -l " + str(packetlength) + " -p " + str(npackets))		
+
+	# Executing the commands
+	for i in tqdm(range(len(commands))):
+		runstring = "xterm -e \"" + commands[i] + "\""
+		print(commands[i])
+		subprocess.run(commands[i])
