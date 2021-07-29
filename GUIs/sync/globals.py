cf067bbfc37acad8fa801b13939ba6a00d3962e3
AndiZm <34646647+AndiZm@users.noreply.github.com>
2021-07-29 15:04:21 +0200

    Add files via upload

--- GUIs/sync/globals.py
+++ GUIs/sync/globals.py
@@ -1,5 +1,10 @@
+# server globals
 connectionLabel = []
 connectionLabelText = ""
 ndevices = 0
 responses = 0
 responsesLabel = ""
+
+# client globals
+stop_wait_for_file_thread = False
+cont_meas_thread = False
