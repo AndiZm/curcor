0fe8b95d9b1096e50490a3574622b1761b8cd7eb
Peter Deiml <peter.deiml@fau.de>
2022-10-24 10:44:39 +0200

    superimpose

--- programs/analysis/superimpose.py
+++ programs/analysis/superimpose.py
@@ -44,7 +44,7 @@ def average_g2s(cA, cB, c3Ax4B, c4Ax3B):
 
 print("Final Analysis of {}".format(star))
 ################################################
-#### Analysis over whole measruement time #####
+#### Analysis over whole measurement time #####
 ################################################
 # Read in the data (g2 functions and time/baseline parameters)
 chAs    = np.loadtxt("g2_functions/{}/ChA.txt".format(star))     
@@ -140,6 +140,11 @@ plt.tight_layout()
 plt.savefig("images/{}_cumulative.png".format(star))
 plt.show()
 
+
+#########################################
+###### Chunk analysis ###################
+#########################################
+
 # Define colormap for plotting all summarized individual g2 functions
 cm_sub = np.linspace(1.0, 0.0, len(chAs))
 colors = [cm.viridis(x) for x in cm_sub]
@@ -169,12 +174,8 @@ ct4_sum = np.zeros(len(ct4s[0]))
 ticks = []
 ffts = []
 
-
-#########################################
-###### Chunk analysis ###################
-#########################################
 # loop over every g2 function chunks
-for i in range(0,len(chAs)):
+for i in range(5,len(chAs)):
     chA = chAs[i]
     chB = chBs[i]
     ct3 = ct3s[i]
@@ -292,26 +293,17 @@ for i in range(0,len(chAs)):
     #plt.errorbar(x, ct4+i*0e-6+1e-5, yerr=0, marker=".", linestyle="--", color = colors[i], alpha=0.6)
     #plt.errorbar(x, ct4+i*0e-6+1e-5, yerr=0, marker=".", linestyle="--", label=timestring, color = colors[i], alpha=0.6)
 
+# store cleaned data
+np.savetxt("g2_functions/{}/ChA_clean.txt".format(star), np.c_[chA_clean], header="{} Channel A cleaned".format(star) )
+np.savetxt("g2_functions/{}/ChB_clean.txt".format(star), np.c_[chB_clean], header="{} Channel B cleaned".format(star) )
+np.savetxt("g2_functions/{}/CT3_clean.txt".format(star), np.c_[ct3_clean], header="{} CT3 cleaned".format(star) )
+np.savetxt("g2_functions/{}/CT4_clean.txt".format(star), np.c_[ct4_clean], header="{} CT4 cleaned".format(star) )
+np.savetxt("g2_functions/{}/C3Ax4B_clean.txt".format(star), np.c_[c3Ax4B_clean], header="{} CT3A x CT4B cleaned".format(star) )
+np.savetxt("g2_functions/{}/C4Ax3B_clean.txt".format(star), np.c_[c4Ax3B_clean], header="{} CT4A x CT3B cleaned".format(star) )
 
 ############################
 # Autocorrelation analysis #
 ############################
-# ---- This is old and not used any more ---- #
-
-# Finalize analysis of integrated autocorrelations and plot it to the auto correlation plot
-# Renormalize ct3 and ct4 autocorrelation data
-ct3_sum = ct3_sum/np.mean(ct3_sum[0:4500])
-ct4_sum = ct4_sum/np.mean(ct4_sum[0:4500])
-# Shift all peaks to zero
-tbin = timebin(118.62); ct3_sum = shift_bins(ct3_sum, tbin)
-tbin = timebin(114.23); ct4_sum = shift_bins(ct4_sum, tbin)
-
-#plt.subplot(224)
-#plt.errorbar(x, ct3_sum, yerr=0, marker=".", linestyle="--", color = "black", linewidth=2, alpha=1)
-#plt.errorbar(x, ct4_sum+1e-5, yerr=0, marker=".", linestyle="--", color = "black", linewidth=2, alpha=1)
-#
-#np.savetxt("autocorrelation_{}.txt".format(star), np.c_[ct3_sum, ct4_sum])
-
 # ---- This is the new method ---- #
 # Read in the autocorrelation functions
 try:
@@ -331,8 +323,6 @@ plt.plot(x_auto, c_auto, color="black")
 plt.plot(xplotf, uti.gauss(xplotf, *popt_avg_free), linestyle="--", color="red")
 plt.ylim(1-1*popt_avg_free[0] , 1+2*popt_avg_free[0])
 
-
-
 # Figure stuff
 def cc_plots(xlims):
     plt.grid()
@@ -348,12 +338,7 @@ plt.yticks(np.arange(1,1+2e-6*len(chAs),2e-6))
 
 plt.subplot(224); plt.title("Auto correlations of {}".format(star)); cc_plots((-100,100))
 
-# store cleaned data
-np.savetxt("g2_functions/{}/ChA_clean.txt".format(star), np.c_[chA_clean], header="{} Channel A cleaned".format(star) )
-np.savetxt("g2_functions/{}/ChB_clean.txt".format(star), np.c_[chB_clean], header="{} Channel B cleaned".format(star) )
-np.savetxt("g2_functions/{}/CT3_clean.txt".format(star), np.c_[ct3_clean], header="{} CT3 cleaned".format(star) )
-np.savetxt("g2_functions/{}/CT4_clean.txt".format(star), np.c_[ct4_clean], header="{} CT4 cleaned".format(star) )
-
+#############################################################
 #### making SC plot (spatial coherence) via integral data ####
 xplot = np.arange(0.1,300,0.1)
 plt.subplot(222)
