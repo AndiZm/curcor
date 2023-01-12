2d3eb9ec064265e16d4deb7a70b51b58ebfe1168
Zmija <of78azyc@fauad.fau.de>
2023-01-12 12:36:10 +0100

    NSB plots with new colors

---
+++ programs/analysis/acrux_autocorrelation.py
@@ -0,0 +1,152 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl  
+from scipy.optimize import curve_fit
+from scipy.signal import butter, filtfilt, find_peaks
+from matplotlib.pyplot import cm
+import ephem
+import scipy.special as scp
+import sys
+
+import utilities as uti
+import corrections as cor
+
+############################################################################
+## take Acrux data and analyze the autocorrelations in different aspects ###
+############################################################################
+star = "Acrux"
+print("Peak part Analysis of {}".format(star))
+
+# Read in the data (g2 functions and time/baseline parameters), only interested in autocorrelations
+chAs  = np.loadtxt("g2_functions/{}/ChA.txt".format(star))
+chBs  = np.loadtxt("g2_functions/{}/ChB.txt".format(star))
+ct3s  = np.loadtxt("g2_functions/{}/CT3_clean.txt".format(star))
+ct4s  = np.loadtxt("g2_functions/{}/CT4_clean.txt".format(star))
+data  = np.loadtxt("g2_functions/{}/baseline.txt".format(star))
+
+# Demo function for initializing x axis and some stuff
+demo = chAs[0]
+x = np.arange(-1.6*len(demo)//2,+1.6*len(demo)//2,1.6)
+
+# There are 24 chunks of Acrux data, so divide both the cross and the auto-correlations in 2x12 to check for systematics
+crossA_0 = np.zeros(len(x)); crossA_1 = np.zeros(len(x))
+crossB_0 = np.zeros(len(x)); crossB_1 = np.zeros(len(x))
+auto3_0  = np.zeros(len(x)); auto3_1  = np.zeros(len(x))
+auto4_0  = np.zeros(len(x)); auto4_1  = np.zeros(len(x))
+for i in range (0,12):
+    crossA_0 += chAs[i]/np.std(chAs[i][0:4500])
+    crossB_0 += chBs[i]/np.std(chBs[i][0:4500])
+    auto3_0  += ct3s[i]/np.std(ct3s[i][0:4500])
+    auto4_0  += ct4s[i]/np.std(ct4s[i][0:4500])
+for i in range (12,24):
+    crossA_1 += chAs[i]/np.std(chAs[i][0:4500])
+    crossB_1 += chBs[i]/np.std(chBs[i][0:4500])
+    auto3_1  += ct3s[i]/np.std(ct3s[i][0:4500])
+    auto4_1  += ct4s[i]/np.std(ct4s[i][0:4500])
+crossA_0 = crossA_0/np.mean(crossA_0[0:4500]); crossA_1 = crossA_1/np.mean(crossA_1[0:4500]) 
+crossB_0 = crossB_0/np.mean(crossB_0[0:4500]); crossB_1 = crossB_1/np.mean(crossB_1[0:4500])
+auto3_0  = auto3_0 /np.mean(auto3_0 [0:4500]); auto3_1  = auto3_1 /np.mean(auto3_1 [0:4500])
+auto4_0  = auto4_0 /np.mean(auto4_0 [0:4500]); auto4_1  = auto4_1 /np.mean(auto4_1 [0:4500])
+
+# Also combine all g2 together
+crossA = ( crossA_0/np.std(crossA_0[0:4500]) + crossA_1/np.std(crossA_1[0:4500]) ); crossA = crossA/np.mean(crossA[0:4500])
+crossB = ( crossB_0/np.std(crossB_0[0:4500]) + crossB_1/np.std(crossB_1[0:4500]) ); crossB = crossB/np.mean(crossB[0:4500])
+auto3  = ( auto3_0 /np.std(auto3_0 [0:4500]) + auto3_1 /np.std(auto3_1 [0:4500]) ); auto3  = auto3 /np.mean(auto3 [0:4500])
+auto4  = ( auto4_0 /np.std(auto4_0 [0:4500]) + auto4_1 /np.std(auto4_1 [0:4500]) ); auto4  = auto4 /np.mean(auto4 [0:4500])
+
+
+# Fit into the combined g2 function for peak integral and parameter fixing ...
+# ... and plot autocorrelations of CT3 and CT4 into one plot to check differences
+# Fit for gaining mu and sigma to fix these parameters
+xplot, popt, perr = uti.fit(auto3, x, 90, 150, 117)
+mu3 = popt[1]; sigma3 = popt[2]
+
+xplot, popt, perr = uti.fit(auto4, x, 90, 150, 117)
+mu4 = popt[1]; sigma4 = popt[2]
+
+#####################################################################################################################
+## Third analysis: take Acrux autocorrelation data analyze chunk bunching peaks quantitatively
+#####################################################################################################################
+# Define colormap for plotting all summarized individual g2 functions
+cm_sub = np.linspace(1.0, 0.0, len(ct3s))
+colors = [cm.viridis(x) for x in cm_sub]
+
+# Define figure which will show all autocorrelations of CT3, CT4 and the fit integrals
+
+plt.figure(figsize=(12,4))
+
+plt.subplot(121)
+
+ints3 = []; dints3 = []; times = []
+ints4 = []; dints4 = []
+
+timestrings = []
+
+# loop over every g2 function chunks
+for i in range(0,len(ct3s)):
+    ct3 = ct3s[i]#[5025:5125]
+    ct4 = ct4s[i]#[5025:5125]
+
+    #x_small = np.arange(-50*1.6,50*1.6,1.6)
+
+    # Apply gaussian fits to auto correlations
+    xplotf, popt3, perr3 = uti.fit_fixed(ct3, x, 80, 160, mu3,sigma3)
+    Int, dInt = uti.integral_fixed(popt3, perr3, sigma3)
+    ints3.append(1e6*Int); dints3.append(1e6*dInt)# in femtoseconds
+
+    xplotf, popt4, perr4 = uti.fit_fixed(ct4, x, 80, 160, mu4,sigma4)
+    Int, dInt = uti.integral_fixed(popt4, perr4, sigma4)
+    ints4.append(1e6*Int); dints4.append(1e6*dInt)# in femtoseconds
+
+    # Check acquisition time of original data
+    timestring = ephem.Date(data[:,0][i])
+    timestrings.append(timestring)
+    print("{}".format(i), timestring, Int, dInt)
+    
+
+    if i%3 == 0:
+        # Subplot for the auto correlation CT3
+        plt.plot(x-mu3, ct3+i*0.5e-6, "o-", label=timestring, color = colors[i], alpha=0.5)
+        plt.plot(xplotf-mu3, uti.gauss_shifted(x=xplotf-mu3, a=popt3[0], mu=0, sigma=sigma3, shift=0.25*i), color=colors[i], linestyle="-")
+    
+        ## Subplot for the auto correlation CT4
+        #plt.subplot(222)
+        #plt.errorbar(x, ct4+i*2e-6, yerr=0, marker=".", linestyle="--", label=timestring, color = colors[i], alpha=0.6)
+        #plt.plot(xplotf, uti.gauss_shifted(x=xplotf, a=popt4[0], mu=mu4, sigma=sigma4, shift=i), color="black", linestyle="-")
+
+    plt.grid()
+    plt.xlim(70-mu3,180-mu3); plt.ylim(1-2e-6, 1+16e-6)
+    plt.ticklabel_format(useOffset=False)
+    plt.legend()
+    plt.tight_layout()
+    plt.xlabel("Time difference (ns)")
+    plt.ylabel("$g^{(2)}$")
+    
+
+    
+
+
+# Figure stuff
+plt.subplot(122)
+
+# plot the peak integrals
+x3 = np.arange(0,len(ints3),1)
+x4 = np.arange(0.1,len(ints4)+0.1,1)
+m3, dm3 = uti.calc_array_mean(ints3, dints3)
+m4, dm4 = uti.calc_array_mean(ints4, dints4)
+plt.title("Peak integrals")
+plt.errorbar(x3, y=ints3, yerr=dints3, marker="o", linestyle="", label="CT 3", color="#8f0303")
+plt.errorbar(x4, y=ints4, yerr=dints4, marker="o", linestyle="", label="CT 4", color="#003366")
+plt.fill_between(x3, y1=np.mean(ints3)+np.std(ints3), y2=np.mean(ints3)-np.std(ints3), color="#8f0303",   alpha=0.1)
+plt.fill_between(x4, y1=np.mean(ints4)+np.std(ints4), y2=np.mean(ints4)-np.std(ints4), color="#003366", alpha=0.1)
+plt.axhline(y=np.mean(ints3), linestyle="--", alpha=0.5, color="#8f0303")
+plt.axhline(y=np.mean(ints4), linestyle="--", alpha=0.5, color="#003366")
+
+plt.xlabel("Measurement index")
+plt.ylabel("Coherence time (fs)")
+#plt.ylim(0,)
+plt.legend()
+plt.tight_layout()
+
+plt.savefig("images/misc/acrux_autocorrelations.pdf")
+plt.show()
--- programs/analysis/nsb/nsb.py
+++ programs/analysis/nsb/nsb.py
@@ -1,56 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import geometry as geo
 
-start = int(0)
-end   = int(9)
+data = np.loadtxt("nsb_data.txt")
 
-# Rate calculations
-off_3a   = np.loadtxt("../calibs_ct3/off.off")[0]
-off_3b   = np.loadtxt("../calibs_ct3/off.off")[1]
-off_4a   = np.loadtxt("../calibs_ct4/off.off")[0]
-off_4b   = np.loadtxt("../calibs_ct4/off.off")[1]
+distances = data[:,0]
+r3as      = data[:,1]
+r3bs      = data[:,2]
+r4as      = data[:,3]
+r4bs      = data[:,4]
 
-calib_3a   = np.loadtxt("../calibs_ct3/calib.calib")[10]
-calib_3b   = np.loadtxt("../calibs_ct3/calib.calib")[11]
-calib_4a   = np.loadtxt("../calibs_ct4/calib.calib")[10]
-calib_4b   = np.loadtxt("../calibs_ct4/calib.calib")[11]
-
-
-def calculate_rates (m3a, m3b, m4a, m4b):
-    rate_3a = 1e-6 * (m3a-off_3a)/(calib_3a * 1.6e-9)
-    rate_3b = 1e-6 * (m3b-off_3b)/(calib_3b * 1.6e-9)
-    rate_4a = 1e-6 * (m4a-off_4a)/(calib_4a * 1.6e-9)
-    rate_4b = 1e-6 * (m4b-off_4b)/(calib_4b * 1.6e-9)
-
-    return rate_3a, rate_3b, rate_4a, rate_4b
-
-
-r3as = []
-r3bs = []
-r4as = []
-r4bs = []
-for i in range (start,end+1):
-    file = "../moon_nsb_{:05d}.fcorr".format(i)
-    tdiff, mean_pc1A, mean_pc1B, mean_pc2A, mean_pc2B, az, alt, thetime = geo.get_params(file)
-    
-    r3a, r3b, r4a, r4b = calculate_rates(mean_pc1A, mean_pc1B, mean_pc2A, mean_pc2B)
-    r3as.append(r3a)
-    r3bs.append(r3b)
-    r4as.append(r4a)
-    r4bs.append(r4b)
-
-x = [10,9,8,7,6,5,4,3,2,1]
-
+plt.figure(figsize=(5,3.5))
 plt.title("Night Sky Background")
-plt.plot(x,r3as, "o--", label="CT3 Ch A", color="red")
-plt.plot(x,r3bs, "o--", label="CT3 Ch B", color="orange")
-plt.plot(x,r4as, "x--", label="CT4 Ch A", color="blue")
-plt.plot(x,r4bs, "x--", label="CT4 Ch B", color="cyan")
+
+plt.plot(distances, r3as, "o--", label="CT3 Ch A", color="#8f0303", markersize=7)
+plt.plot(distances, r3bs, "o--", label="CT3 Ch B", color="#f7a488", markersize=7)
+plt.plot(distances, r4as, "X--", label="CT4 Ch A", color="#003366", markersize=7)
+plt.plot(distances, r4bs, "X--", label="CT4 Ch B", color="#98cced", markersize=7)
 plt.xlabel("Separation from moon ($^\circ$)")
 plt.ylabel("Photon rates (MHz)")
 plt.legend()
-plt.grid()
-plt.xlim(10,0)
-plt.savefig("nsb.png")
+#plt.grid()
+plt.xlim(11,0)
+plt.tight_layout()
+plt.savefig("nsb.pdf")
 plt.show()
