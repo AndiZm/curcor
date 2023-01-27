b9ad63743e92a03fded2b89125022f4c4b5aed9e
Vogel <az10opyl@fauad.fau.de>
2023-01-27 11:48:36 +0100

    rate plots paper

--- programs/analysis/rates_paper_plot.py
+++ programs/analysis/rates_paper_plot.py
@@ -25,22 +25,24 @@ rates_3A= np.loadtxt( "rates/{}/{}_{}.txt".format(star, star, date) )[:,1]
 rates_3B= np.loadtxt( "rates/{}/{}_{}.txt".format(star, star, date) )[:,2]
 
 tplot=[]
+tplots=[]
 for i in range(len(times)):
 	time = (ephem.Date(times[i]))
 	t = str(time) 
 	tplot.append(t.split(' ')[1])	# get h:min:sec
-print(tplot[-1])
+	tplots.append(tplot[-1][0:5])
+print(tplots[-1])
 
 
-F1 = plt.figure(figsize=(10, 5))
-plt.plot(tplot, rates_3A, color=uti.color_3A, label='Channel 3A', marker=".")
-plt.plot(tplot, rates_3B, color=uti.color_3B, label="Channel 3B", marker='.')
-plt.xticks(tplot[::1000], rotation=45)
-#plt.yticks(fontsize=13)
-plt.legend()
-plt.title("Rates of {}".format(star))
-plt.xlabel("Time (UTC)")
-plt.ylabel("Rate (MHz)")
+F1 = plt.figure(figsize=(7, 5))
+plt.plot(tplots, rates_3A, color=uti.color_3A, label='Channel 3A', marker=".")
+plt.plot(tplots, rates_3B, color=uti.color_3B, label="Channel 3B", marker='.')
+plt.xticks(tplots[::1000], rotation=45,fontsize=12)
+plt.yticks(fontsize=12)
+plt.legend(fontsize=12)
+plt.title("Rates of {}".format(star), fontsize=15)
+plt.xlabel("Time (UTC)",fontsize=13)
+plt.ylabel("Rate (MHz)",fontsize=13)
 plt.tight_layout()
 plt.savefig("rates/{}/{}_{}_paper.pdf".format(star, star, date))
 plt.show()
