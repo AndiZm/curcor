217e142e649efe5ad9f12f59a2b088ff3d2ddd86
Zmija <of78azyc@fauad.fau.de>
2024-02-26 15:10:43 +0100

    testing some stuff

---
+++ programs/analysis/2023/analysis_chunk3T_demo.py
@@ -0,0 +1,195 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.dates as mdates
+from matplotlib.pyplot import cm
+import matplotlib as mpl
+from tqdm import tqdm
+from datetime import datetime, timezone
+import ephem
+import sys
+import os
+
+import geometry as geo
+import corrections as cor
+import utilities as uti
+
+from threading import Thread
+
+star = sys.argv[1]
+
+# Define files to analyze and subpackages
+folders   = [] # Data folders for analysis
+stepsizes = [] # Number of files for a single g2 function
+ends      = [] # Total number of files used from this folder
+telcombis = [] # Combination of telescopes (either 14, 34 or 134)
+
+# Open text file with measurement split data and read the data for the specific star
+f = open("measurement_chunks.txt")
+# Find line for the star
+line = f.readline()
+while star not in line:
+    line = f.readline()
+# Fill arrays with the data
+line = f.readline()
+while "[end]" not in line:
+    folders.append(line.split()[0])
+    stepsizes.append( int(line.split()[1]) )
+    ends.append( int(line.split()[2]) )
+    telcombis.append( str(line.split()[3]) )
+    line = f.readline()
+f.close()
+
+print ("Analyzing {}".format(star))
+
+star_small = star[0].lower() + star[1:]
+# Get the timebin shift of the specific measurement from the time difference
+def timebin(tdiff):
+    return int(1.0* np.floor((tdiff+0.8)/1.6))
+def shift_bins(data, binshift):
+    # A negative number means shifting to the right, so scrapping the right end of the array to the beginning
+    if binshift <= 0:
+        for j in range (binshift,0):
+            data = np.insert(data,0,data[-1])
+            data = np.delete(data, -1)
+    # A positive number means shifting to the left, so scrapping the beginning of the array to the end
+    if binshift > 0:
+        for j in range (0, binshift):
+            data = np.append(data,data[0])
+            data = np.delete(data, 0)
+    return data
+def get_baseline_entry(telcombi):
+    if telcombi == "13":
+        return int(0)
+    elif telcombi == "14":
+        return int(1)
+    elif telcombi == "34":
+        return int(2)
+
+# -------------------------------------------------- #
+# -- Initialize parameter arrays for data storing -- #
+# Baselines and uncertainties for each telescope combination
+baselines  = np.zeros((5,5), dtype=object)
+dbaselines = np.zeros((5,5), dtype=object)
+# Acquisition times
+ac_times = np.zeros((5,5), dtype=object)
+# Every index is an empty list at the beginning
+for i in range(5):
+    for j in range(5):
+        baselines[i,j] = []
+        dbaselines[i,j] = []
+        ac_times[i,j] = []
+# -------------------------------------------------- #
+
+
+# Number of datapoints
+N = 2 * 1024**3 # 2G sample file
+folderpath = "C:/Users/ii/Documents/curcor/corr_results/results_HESS"
+
+def corr_parts(folder, start, stop, telcombi):
+    # Define the filetype based on the telcombi. Historically, measurements with two telescopes have the filetype "fcorr6", while measurements with 3 telescopes have "fcorr3T"
+    if len(telcombi) == 2:
+        filetype = "fcorr6"
+    elif len(telcombi) == 3:
+        filetype = "fcorr3T"
+    else:
+        print ("Arrrgh! The filetype for your telcombi does not exist!")
+        exit(0)
+
+    # Fill the relevant entries
+    telpairs = [] # This will be a list of the existing telescope combinations. For "134" e.g. the list will be [[1,3],[1,4],[3,4]]
+    for i in range(1,5):
+        # Now initialize the g2 functions of the participating telescope pairs
+        for j in range (1,5):
+            if j > i and str(i) in telcombi and str(j) in telcombi:
+                print ("\tIntialize telescope combination {}-{}".format(i,j))
+                telpairs.append([i,j])
+
+    times = [] # Will be filled with individual time stampes to find the central acquisition time for the chunk
+    baseline_values = [] # TODO: wahrscheinlich aendern
+
+    # Initialize baseline matrix
+    baseline  = np.zeros((5,5), dtype=object)
+    dbaseline = np.zeros((5,5), dtype=object) # for the uncertainties
+    for k in range(0,5):
+        for l in range(0,5):
+            baseline[k,l]  = []
+            dbaseline[k,l] = []
+
+    # Loop over every file
+    #del# for i in tqdm(range ( 0,len(files) )):
+    for i in tqdm(range ( start, stop )):
+        # Read in the header information
+        data = np.loadtxt("{}/{}/{}_{:05d}.header".format(folderpath, folder, star, i))
+        time = datetime.utcfromtimestamp(data[0]); times.append( ephem.Date(time) )
+
+        # Loop over every telescope pair
+        for pair in telpairs:
+            pairstring = str(pair[0]) + str(pair[1])
+    
+            # Get file parameters from header and ephem calculations
+            if star == "Regor":
+                tdiff, az, alt = geo.get_params_manual3T(time, ra=[8,10,12.5], dec=[-47,24,22.2], telcombi=pairstring)
+            elif star == "Etacen":
+                tdiff, az, alt = geo.get_params_manual3T(time, ra=[14,35,30.42], dec=[-42,9,28.17], telcombi=pairstring)
+            elif star == "Dschubba":
+                tdiff, az, alt = geo.get_params_manual3T(time, ra=[16,0,20], dec=[-22,37,18.14], telcombi=pairstring)
+            else:
+                tdiff, az, alt = geo.get_params3T(time, starname=star, telcombi=pairstring)
+
+            # Store baseline
+            baseline[pair[0],pair[1]].append( uti.get_baseline3T(date=time, star=star, telcombi=pairstring) )
+        
+
+    ##################################
+    # Finish things up for the chunk #
+    ##################################
+    time_mean = np.mean(times)
+    print ("Central time = {}".format(ephem.Date(time_mean)))
+    
+    # Calculate mean baseline and baseline error
+    for i in range(5):
+        for j in range(5):
+            dbaseline[i,j] = np.nanstd(baseline[i,j])  # first the error, bc the baseline array will be changed in the next line
+            baseline[i,j]  = np.nanmean(baseline[i,j]) # this transfers the array of lists into a simple array with the mean baseline
+
+    print ("Baselines:")
+    print (baseline)
+    print ("Baseline errors:")
+    print (dbaseline)
+
+    # save data into correct arrays
+    for pair in telpairs:
+        # append baselines and uncertainties to the matrix of telescope baseline lists
+        baselines[pair[0],pair[1]].append(baseline[pair[0],pair[1]])
+        dbaselines[pair[0],pair[1]].append(dbaseline[pair[0],pair[1]])
+        # append acquisition times array. Even though they are the same for each combination in this chunk, there may be different runs with different telescope combinations
+        ac_times[pair[0],pair[1]].append(time_mean)
+
+##########################################
+# Add the number of files to be analyzed #
+for i in range(len(folders)):
+    folder   = folders[i]
+    stepsize = stepsizes[i]
+    end      = ends[i]
+    telcombi = telcombis[i]
+
+    steps = np.arange(0, end + 1, stepsize)
+    for j in range(len(steps)-1):
+        start = steps[j]
+        stop = steps[j+1]
+        corr_parts(folder, start, stop, telcombi)
+
+
+
+# Create folder for stars, if not already existing
+os.makedirs("testing/g2_functions/{}".format(star), exist_ok=True)
+
+for i in range(5):
+    for j in range(5):
+        if len( ac_times[i,j] ) > 0: # this combination exists
+            # create combination folder, if not already existing
+            os.makedirs("testing/g2_functions/{}/{}{}".format(star,i,j), exist_ok=True)
+            # save all the data
+            np.savetxt("testing/g2_functions/{}/{}{}/ac_times.txt".format(star,i,j), np.c_[ ac_times[i,j] ])
+            np.savetxt("testing/g2_functions/{}/{}{}/baselines.txt".format(star,i,j), np.c_[ baselines[i,j] ])
+            np.savetxt("testing/g2_functions/{}/{}{}/dbaselines.txt".format(star,i,j), np.c_[ dbaselines[i,j] ])
---
+++ programs/analysis/2023/geometry_3T.py
@@ -0,0 +1,156 @@
+import ephem
+import os
+from datetime import datetime, timezone
+import numpy as np
+
+def get_baseline_entry(telcombi):
+    if telcombi == "13":
+        return int(0)
+    elif telcombi == "14":
+        return int(1)
+    elif telcombi == "34":
+        return int(2)
+
+# We are going to cartesian coordinates
+# HESS coordinates (already transfered so that x is west-east and y is south-north)
+ct1 = [ 85.04, -0.16,  0.97]
+ct2 = [  0.37, 85.07,  0.33]
+ct3 = [-85.04,  0.24, -0.82]
+ct4 = [ -0.28,-85.04, -0.48]
+# with these four telescope vectors we build the list of vectors
+# we add an additional nan as 0th element, so that we can attribute each index with the corresponding telescope
+ct_locs = [[np.nan], ct1, ct2, ct3, ct4]
+# Now we can build the matrix of 3D-telescope seperations
+# Each entry is the 3D seperation vector
+teldiffs = np.zeros((5,5), dtype=object)
+for i in range (1,5):
+	for j in range (1,5):
+		teldiffs[i][j] = np.subtract(ct_locs[j], ct_locs[i])
+
+# Define plane of incident light from star by normal vector from the telescopes in direction of star
+# For the baseline, we define the plane perpendicular to that one
+n_vec   = [] # for the optical path delay
+def get_plane_vector(az,alt):
+	global n_vec
+	phi = np.pi - az
+
+	# -- First for the optical path delay -- #
+	x = + np.sin(phi)
+	y = - np.cos(phi)
+	z = + np.tan(alt)
+	# Normalize normal vector
+	length = np.sqrt(x**2+y**2+z**2)
+	x /= length; y /= length; z /= length
+	n_vec = [x,y,z]
+
+# Calculate distance of telescope T2 to plane == distance difference of travelled light
+def get_distance(telcombi):
+	# Distance is scalar product of normal vector and T2 vector
+	d = -1 * np.dot(n_vec, teldiffs[telcombi[0]][telcombi[1]])
+	# ... but here for d>0 light is incident on T2 first, therefore the -1
+	# Now, negative d means light is incident on T2 first, positive d means light is incident on T1 first
+	return d
+
+# Calculate the projected baseline
+def get_baseline(telcombi):
+	# Projection of vector onto plane
+	# Start with the projection of a vector onto the norm vector
+	proj  = np.multiply( np.dot(teldiffs[telcombi[0]][telcombi[1]], n_vec),n_vec )
+	# The projection onto the plane is the vector minus the projection
+	projP = np.subtract( teldiffs[telcombi[0]][telcombi[1]], proj)
+	# normalize
+	b = np.linalg.norm(projP)	
+	return b
+
+# Time delay between the two telescopes at any UTC time
+def get_time_delay_azalt(az, alt, telcombi):
+	get_plane_vector(az, alt)
+	d = get_distance(telcombi)
+	b = get_baseline(telcombi)
+	# Time difference
+	t = 1e9*d/299792458 # nanoseconds
+	return t # nanoseconds
+
+#########################
+### STAR CALCULATIONS ###
+#########################
+# Ephem parameters
+#the_star = ephem.star("Shaula")
+
+hess = ephem.Observer()
+hess.lat  = ephem.degrees("-23.271778")
+hess.long = ephem.degrees(" 16.50022")
+#hess.date = ephem.now()
+
+def get_params(file, starname, telcombi):
+	# Open file
+	f = open(file)
+	# Read in header, which is the first line of the file
+	header = f.readline().split(" ")
+	ctime  = float(header[1])
+	mean_1 = float(header[2])
+	mean_2 = float(header[3])
+	mean_3 = float(header[4])
+	mean_4 = float(header[5])
+	f.close()
+	# Star coordinates
+	the_star = ephem.star(starname)
+	time = datetime.utcfromtimestamp(ctime)
+	hess.date = ephem.date(time)
+
+	the_star.compute(hess)
+	# Time delay between the telescopes
+	tdiff = get_time_delay_azalt(the_star.az, the_star.alt)[get_baseline_entry(telcombi)]
+
+	return tdiff, mean_1, mean_2, mean_3, mean_4, 180*the_star.az/np.pi, 180*the_star.alt/np.pi, time
+
+def get_params_manual(file, ra, dec, telcombi):
+	# Open file
+	f = open(file)
+	# Read in header, which is the first line of the file
+	header = f.readline().split(" ")
+	ctime  = float(header[1])
+	mean_1 = float(header[2])
+	mean_2 = float(header[3])
+	mean_3 = float(header[4])
+	mean_4 = float(header[5])
+	f.close()
+	# Star coordinates
+	the_star = ephem.FixedBody()
+	the_star._ra  = ephem.hours("{}:{}:{}".format(ra[0],ra[1],ra[2]))
+	the_star._dec = ephem.degrees("{}:{}:{}".format(dec[0],dec[1],dec[2]))
+	time = datetime.utcfromtimestamp(ctime)
+	hess.date = ephem.date(time)
+
+	the_star.compute(hess)
+	# Time delay between the telescopes
+	tdiff = get_time_delay_azalt(the_star.az, the_star.alt)[get_baseline_entry(telcombi)]
+
+	return tdiff, mean_1, mean_2, mean_3, mean_4, 180*the_star.az/np.pi, 180*the_star.alt/np.pi, time
+
+
+# 3 Telescopes
+
+def get_params3T(time, starname, telcombi):
+	# Star coordinates
+	the_star = ephem.star(starname)
+	hess.date = ephem.date(time)
+
+	the_star.compute(hess)
+	# Time delay between the telescopes
+	tdiff = get_time_delay_azalt(the_star.az, the_star.alt)[get_baseline_entry(telcombi)]
+
+	return tdiff, 180*the_star.az/np.pi, 180*the_star.alt/np.pi
+
+def get_params_manual3T(time, ra, dec, telcombi):
+	# Star coordinates
+	the_star = ephem.FixedBody()
+	the_star._ra  = ephem.hours("{}:{}:{}".format(ra[0],ra[1],ra[2]))
+	the_star._dec = ephem.degrees("{}:{}:{}".format(dec[0],dec[1],dec[2]))
+	hess.date = ephem.date(time)
+
+	the_star.compute(hess)
+	# Time delay between the telescopes
+	tdiff = get_time_delay_azalt(the_star.az, the_star.alt)[get_baseline_entry(telcombi)]
+
+	return tdiff, 180*the_star.az/np.pi, 180*the_star.alt/np.pi
